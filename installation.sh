36dfb05d225a758b0be9b33c6a98b33cc93dd62c
b23prodtm@users.sourceforge.net <brunotr@localhost.localdomain>
2026-01-09 22:50:06 +0100

    Pod specs rootless:

--- installation.sh
+++ installation.sh
@@ -13,24 +13,24 @@ HELM_VERSION="v1.6.1"
 HELM_INSTALL=0x1
 KUBERNETES_INSTALL=0x10
 KUBERNETES_UPGRADE=0x100
-(( INSTALL=KUBERNETES_INSTALL ))
+(( GATEWAY_INSTALL=KUBERNETES_INSTALL ))
 printf "%s\n" ""
 printf "%s\n" "Enable rerun job with SSH..."
 printf "%s\n" "Envoyproxy/gateway-helm version $HELM_VERSION"
 kubectl uninstall eg -n envoy-gateway-system|| true
 kubectl delete namespace envoy-gateway-system || true
 
-if (( INSTALL & HELM_INSTALL )); then
+if (( GATEWAY_INSTALL & HELM_INSTALL )); then
   printf "%s\n" "Installation with Helm..."
   helm install eg oci://docker.io/envoyproxy/gateway-helm --version "$HELM_VERSION" -n envoy-gateway-system --create-namespace
   kubectl wait --timeout=5m -n envoy-gateway-system deployment/envoy-gateway --for=condition=Available
   kubectl apply -f https://github.com/envoyproxy/gateway/releases/download/latest/quickstart.yaml -n default
 fi
-if (( INSTALL & KUBERNETES_INSTALL )); then
+if (( GATEWAY_INSTALL & KUBERNETES_INSTALL )); then
   printf "%s\n" "Installation with Kubernetes..."
   kubectl apply --force-conflicts --server-side -f https://github.com/envoyproxy/gateway/releases/download/latest/install.yaml
 fi
-if (( INSTALL & KUBERNETES_UPGRADE )); then  
+if (( GATEWAY_INSTALL & KUBERNETES_UPGRADE )); then  
   helm pull oci://docker.io/envoyproxy/gateway-helm --version "$HELM_VERSION" --untar
   kubectl apply --force-conflicts --server-side -f ./gateway-helm/crds/gatewayapi-crds.yaml
   kubectl apply --force-conflicts --server-side -f ./gateway-helm/crds/generated
