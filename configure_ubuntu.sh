d97b9b07bea66961cca8d45c856084ddfd05d16e
B T R Xn <b23prodtm@users.sourceforge.net>
2026-01-11 05:48:51 +0100

    continue script..

--- configure_ubuntu.sh
+++ configure_ubuntu.sh
@@ -165,6 +165,7 @@ main() {
         
         # Update package list
         sudo apt-get update
+	sudo apt-get install curl
         curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 	sudo dpkg -i minikube_latest_amd64.deb
 
